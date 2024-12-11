2e8e6f59e507811849f5519526b67778262840e6
AsanovAlbek <87094098+AsanovAlbek@users.noreply.github.com>
2024-12-11 21:40:57 +0300

    I know what Im doing, only for github actions

---
+++ lib/core/constants/private.dart
@@ -0,0 +1,4 @@
+const String balRusBaseUrl = 'http://balrusapi.kbncran.ru/';
+const String testBaseUrl = 'http://192.168.177.2:8000/';
+const String reserveBalRusApiUrl = 'https://balrus-dict-api.onrender.com/';
+const String singleSettingsKey = 'AppSettings';
